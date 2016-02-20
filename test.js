5a807b50136054a510e845cd962bdf9c2e7eb78b
deathcap <deathcap@gmx.co.uk>
2016-02-20 00:49:48 -0800

    Test or

--- test.js
+++ test.js
@@ -19,3 +19,7 @@ test('and msb', t => {
   t.deepEqual(bitwise2x32.and([0x11223344, 0xaabbccdd], [0xf0000000, 0xff0000ff]), [0x10000000, 0xaa0000dd]);
   t.end();
 });
+test('or', t => {
+  t.deepEqual(bitwise2x32.or([0x11003300, 0xaa00cc00], [0x00220044, 0x00bb00dd]), [0x11223344, 0xaabbccdd]);
+  t.end();
+});
