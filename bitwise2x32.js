c3c7f3509e497ff846f52f85d012878c5e200c5b
deathcap <deathcap@gmx.co.uk>
2016-02-20 00:42:50 -0800

    Fix signed and test

--- bitwise2x32.js
+++ bitwise2x32.js
@@ -1,7 +1,17 @@
 'use strict';
 
+// Convert an signed 32-bit result (from bitwise operations) to
+// JavaScript's 53-bit numeric type as if it was unsigned
+function signed32toUnsigned53(n) {
+  if (n < 0) {
+    return 0x100000000 + n;
+  } else {
+    return n;
+  }
+}
+
 function and(a, b) {
-  return [a[0] & b[0], a[1] & b[1]];
+  return [a[0] & b[0], signed32toUnsigned53(a[1] & b[1])];
 }
 
 module.exports = {
