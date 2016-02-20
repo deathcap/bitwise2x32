ff860fce39d6185b597fa2132a16566ea6028b06
deathcap <deathcap@gmx.co.uk>
2016-02-20 00:30:28 -0800

    Add (failing) and test with MSB set

--- test.js
+++ test.js
@@ -4,6 +4,10 @@ const test = require('tape');
 const bitwise2x32 = require('./');
 
 test('and', t => {
-  t.deepEqual(bitwise2x32.and([0x11223344, 0x55667788], [0xf0000000, 0x000000ff]), [0x10000000, 0x00000088]);
+  t.deepEqual(bitwise2x32.and([0x11223344, 0x55667788], [0xf0000000, 0xff0000ff]), [0x10000000, 0x55000088]);
+  t.end();
+});
+test('and msb', t => {
+  t.deepEqual(bitwise2x32.and([0x11223344, 0xaabbccdd], [0xf0000000, 0xff0000ff]), [0x10000000, 0xaa0000dd]);
   t.end();
 });
